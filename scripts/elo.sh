d5b338f46a41b29b33ad178dce65ce0a8e0cbd12
Jiangzq <sxdxjzq@gmail.com>
2026-03-29 21:13:38 +0800

    Update RESULTS_ROOT path in elo.sh script

--- scripts/elo.sh
+++ scripts/elo.sh
@@ -14,7 +14,7 @@ PYTHON_BIN=${PYTHON_BIN:-python3}
 BOOTSTRAP=${1:-100}
 EXCLUDE_MODELS=${2:-}
 
-RESULTS_ROOT="${REPO_ROOT}/data/e_openedit_pair_res/openedit"
+RESULTS_ROOT="${REPO_ROOT}/data/e_geditv2_pair_res/geditv2"
 IF_RESULTS_PATH="${RESULTS_ROOT}/eval_if_metadata/example.jsonl"
 VQ_RESULTS_PATH="${RESULTS_ROOT}/eval_vq_metadata/example.jsonl"
 VC_RESULTS_PATH="${RESULTS_ROOT}/eval_vc_metadata/example.jsonl"
